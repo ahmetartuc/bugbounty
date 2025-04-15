4e2b38ed5f56f2546d0f26f4e1e0339c1e1b06d0
Ahmet Artuç <146843472+ahmetartuc@users.noreply.github.com>
2025-04-15 19:34:37 +0300

    Update cors-checker.sh

--- scripts/cors-checker.sh
+++ scripts/cors-checker.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 subdomain_file="subdomains.txt"
-
-evil_origin="http://evil.com"
-
-output_file="cors_results.txt"
-> "$output_file"  # clear previous content
+evil_origin="https://evil.com"
 
 while read -r subdomain; do
     echo "[*] Testing $subdomain"
@@ -13,9 +9,9 @@ while read -r subdomain; do
     response=$(curl -s -i -H "Origin: $evil_origin" -H "Host: $subdomain" "https://$subdomain" --max-time 10)
 
     if echo "$response" | grep -qi "Access-Control-Allow-Origin: $evil_origin"; then
-        echo "[+] POTENTIAL CORS VULNERABILITY: $subdomain" | tee -a "$output_file"
+        echo "[+] POTENTIAL CORS VULNERABILITY: $subdomain"
     fi
 
 done < "$subdomain_file"
 
-echo "[*] Scan complete. Results saved in $output_file"
+echo "[*] Scan complete."
